1568ac2f3a583c63a289ef0b28701901d7a4fb83
William Santos <williamdsw@outlook.com>
2020-09-20 13:21:42 -0300

    Creating Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/home';
 
 // JSX - Writing HTML tags within JS Script
 function App() {
   return (
     <div>
       <Header/>
+      <Home />
     </div>
   );
 }
